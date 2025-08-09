0d0598d2ea07a2cdc5e65384880c589a267221a2
Christabel Edumadze <caedumadze@st.ug.edu.gh>
2025-08-09 12:35:42 +0000

    Task 2: Abstract Classes and Methods

--- DCIT318_Assignment2/AbstractDemo/Program.cs
+++ DCIT318_Assignment2/AbstractDemo/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace AbstractDemo
+{
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; }
+        public Circle(double radius) => Radius = radius;
+        public override double GetArea() => Math.PI * Radius * Radius;
+    }
+
+    class Rectangle : Shape
+    {
+        public double Width { get; }
+        public double Height { get; }
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public override double GetArea() => Width * Height;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Abstract Classes and Methods Demo ===");
+
+            Shape circle = new Circle(3.5);
+            Shape rectangle = new Rectangle(4, 5);
+
+            Console.WriteLine($"Circle (r=3.5) area: {circle.GetArea():F2}");
+            Console.WriteLine($"Rectangle (w=4, h=5) area: {rectangle.GetArea():F2}");
+        }
+    }
+}
