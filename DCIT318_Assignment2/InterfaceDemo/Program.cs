17a4884304491b6e223102fe8af4aee0dd38fb55
Christabel Edumadze <caedumadze@st.ug.edu.gh>
2025-08-09 12:43:16 +0000

    Task 3: Interfaces

--- DCIT318_Assignment2/InterfaceDemo/Program.cs
+++ DCIT318_Assignment2/InterfaceDemo/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace InterfaceDemo
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move() => Console.WriteLine("Car is moving");
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move() => Console.WriteLine("Bicycle is moving");
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Interfaces Demo ===");
+
+            IMovable car = new Car();
+            IMovable bike = new Bicycle();
+
+            car.Move();
+            bike.Move();
+        }
+    }
+}
